2c3ffa3c7bbd92842a5112b10094d6c7d2abf6a7
ElBrunoX.js <167462288+DaSilvaFelix@users.noreply.github.com>
2025-08-21 13:43:11 -0300

    Update 10-herenciaDeClase.ts

--- src/10-herenciaDeClase.ts
+++ src/10-herenciaDeClase.ts
@@ -1,23 +1,23 @@
-// class Car {
-//   private make: string;
-//   constructor(make: string) {
-//     this.make = make;
-//   }
+ class Car {
+   private make: string;
+   constructor(make: string) {
+     this.make = make;
+   }
 
-//   driver(): string {
-//     return `Driving a ${this.make}`;
-//   }
-// }
+   driver(): string {
+     return `Driving a ${this.make}`;
+   }
+ }
 // // ---------------------------------------------------------
-// class ElectricCar extends Car {
-//   constructor(make: string) {
-//     super(make);
-//   }
+ class ElectricCar extends Car {
+   constructor(make: string) {
+     super(make);
+   }
 
-//   charge(): string {
-//     return `el coche se está cargando.`;
-//   }
-// }
+   charge(): string {
+     return `el coche se está cargando.`;
+   }
+ }
 
-// const myCar = new ElectricCar("Tesla");
-// console.log(myCar.driver());
+ const myCar = new ElectricCar("Tesla");
+ console.log(myCar.driver());
